011eff3f250ec911b56803e2a5681a7163ecff05
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-05-18 13:54:18 -0400

    Update ShoppingCartMvcApplicationTests.java

--- ShoppingCartMVC/src/test/java/com/main/ShoppingCartMvcApplicationTests.java
+++ ShoppingCartMVC/src/test/java/com/main/ShoppingCartMvcApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShoppingCartMvcApplicationTests {
+class ShoppingCartMVCApplicationTests {
 
 	@Test
 	void contextLoads() {
