45e672181fc8a6fd383093437a08935b6f113846
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-05-19 15:55:50 -0400

    Update itemController.java

--- ShoppingCartMVC/src/main/java/com/main/item/controller/itemController.java
+++ ShoppingCartMVC/src/main/java/com/main/item/controller/itemController.java
@@ -1,5 +1,47 @@
 package com.main.item.controller;
 
-public class itemController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.main.cart.model.Cart;
+import com.main.item.model.Item;
+import com.main.item.repo.ItemRepo;
+
+@Controller
+
+@RequestMapping("/item")
+public class ItemController {
+	@Autowired
+	ItemRepo repo;
+	@Autowired
+	Cart cart;
+	
+	@GetMapping({"/getItems","/"})
+	public ModelAndView getItems() {
+		ModelAndView mav = new ModelAndView("ItemList");
+		mav.addObject("items",repo.findAll());
+		mav.addObject("cartSize",cart.toString());
+		return mav;
+	}
+	
+	@RequestMapping("/addItem")
+	public ModelAndView addItemPage() {
+		ModelAndView mav = new ModelAndView("AddItem");
+		
+		mav.addObject("item",new Item());
+		return mav;
+	}
+	
+	@RequestMapping("/saveItem")
+	public String addItem(@ModelAttribute Item item) {
+		item.setTotalCost();
+		repo.save(item);
+		
+		return "redirect:addItem";
+	}
 
 }
