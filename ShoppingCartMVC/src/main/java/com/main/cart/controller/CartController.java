f20284e6b3889f732842b4ddd9f89086e8ba57ed
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-05-22 10:58:20 -0400

    Update CartController.java
    changed the /addtocart mapping to include an {id} at the end to take in which item is to be added to the cart

--- ShoppingCartMVC/src/main/java/com/main/cart/controller/CartController.java
+++ ShoppingCartMVC/src/main/java/com/main/cart/controller/CartController.java
@@ -2,11 +2,15 @@ package com.main.cart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.main.cart.model.Cart;
 import com.main.item.model.Item;
+import com.main.item.repo.ItemRepo;
 
 @Controller
 @RequestMapping("/cart")
@@ -14,13 +18,27 @@ public class CartController {
 	
 	@Autowired
 	Cart cart;
+	@Autowired
+	Item item;	
+	@Autowired
+	ItemRepo repo;
 	
-	@RequestMapping("/addToCart")
-	public String addItemToCart(@ModelAttribute Item item) {
+	
+	
+	@RequestMapping("/addToCart/{id}")
+	public String addItemToCart(@PathVariable("id") int  id) {
+		Item item = repo.getById(id);		
+		System.out.println(item);
 		cart.addItem(item);
-		System.out.println(cart.getList().size());
-		System.out.println("method ran");
 		return "redirect:/item/";
+	}
+	
+	@RequestMapping("/checkOut")
+	public ModelAndView goToCheckOut() {
+		ModelAndView mav = new ModelAndView("CheckOut");
+		mav.addObject("items",cart.getList());
+		mav.addObject("cartPrice",cart.getTotalPrice());
+		return mav;
 	}
 	
 }
