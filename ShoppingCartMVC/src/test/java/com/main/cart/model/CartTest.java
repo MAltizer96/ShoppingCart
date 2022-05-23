d5c4361b70fd6e6034c2cbf88963a5aa10c8f3fa
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-05-23 13:13:06 -0400

    Create CartTest.java

---
+++ ShoppingCartMVC/src/test/java/com/main/cart/model/CartTest.java
@@ -0,0 +1,19 @@
+package com.main.cart.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CartTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
