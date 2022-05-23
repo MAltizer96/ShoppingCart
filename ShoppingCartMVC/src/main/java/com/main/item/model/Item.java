8f5aac36fdcc723159ed66eedd0575d989959a94
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-05-23 08:47:43 -0400

    Update Item.java
    -added source equals and hashCode methods

--- ShoppingCartMVC/src/main/java/com/main/item/model/Item.java
+++ ShoppingCartMVC/src/main/java/com/main/item/model/Item.java
@@ -1,9 +1,12 @@
 package com.main.item.model;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -39,28 +42,33 @@ public class Item {
 		totalCost = value;
 	}
 	
-	@Override
-	public boolean equals(Object object) {
-		if(object != Item.class) {
-			return false;
-		}
-		
-		Item compareItem = (Item)object;
-		if(this.getName().equals(compareItem.name)&&
-		   this.getTotalCost() == compareItem.getTotalCost()&&
-		   this.getType().equals(compareItem.getType())&&
-		   this.getWeight() == compareItem.getWeight()
-				) {
-			
-			return true;
-		}
-		return false;
-	}
+	
 
 	@Override
 	public String toString() {
 		return "Item [id=" + id + ", name=" + name + ", totalCost=" + totalCost + ", weight=" + weight + ", type="
 				+ type + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		return costChange == other.costChange && costDollars == other.costDollars && id == other.id
+				&& Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(totalCost) == Double.doubleToLongBits(other.totalCost)
+				&& Objects.equals(type, other.type)
+				&& Double.doubleToLongBits(weight) == Double.doubleToLongBits(other.weight);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(costChange, costDollars, id, name, totalCost, type, weight);
 	}
 	
 	
