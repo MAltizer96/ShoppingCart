8f03bca169ed15e6a59a6de4d9496579e90794c6
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-05-23 13:49:00 -0400

    Update CartTest.java
    -updated the setUp method to create a new cart and add items to it
    -created the totalNumberOfItemsInCartTest method

--- ShoppingCartMVC/src/test/java/com/main/cart/model/CartTest.java
+++ ShoppingCartMVC/src/test/java/com/main/cart/model/CartTest.java
@@ -1,19 +1,31 @@
 package com.main.cart.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.main.item.model.Item;
+
 class CartTest {
+
+	Cart cart = null;
 
 	@BeforeEach
 	void setUp() throws Exception {
+
+		cart = new Cart();
+		cart.addItem(new Item("Soup", 0, 99, 0.2));
+		cart.addItem(new Item("Soup", 0, 99, 0.2));
+		cart.addItem(new Item("Soup", 0, 99, 0.2));
+		cart.addItem(new Item("Candy", 1, 99, 1));
+		cart.addItem(new Item("Candy", 1, 99, 1));
+		cart.addItem(new Item("Onion", 5, 99, 5));
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void totalNumberOfItemsInCartTest() {
+		assertEquals(2, cart.totalNumberOfItemsInCart(new Item("Candy", 1, 99, 1)));
 	}
 
 }
