0dc80cb584957ee194d339191669444ee27e84bd
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-05-25 14:38:47 -0400

    Update Item.java
    -removed int varaiables for dollar cost and cent cost
    -changed totalCost variable name into totalPrice & method and constructors updated to match
    -removed equals and hashCode methods

--- ShoppingCartMVC/src/main/java/com/main/item/model/Item.java
+++ ShoppingCartMVC/src/main/java/com/main/item/model/Item.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.stereotype.Component;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,67 +15,53 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @NoArgsConstructor
+@Component
 public class Item {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
-	private String name;
-	private int costDollars;
-	private int costChange;
-	private double totalCost;
+	private String name;	
+	private double totalPrice;
 	private double weight;
 	private String type;
 	
-	
-	public Item(String name, int costDollars, int costChange, double weight) {
+	public Item(int id,String name, double price, double weight) {
 		super();
+		this.id=id;
 		this.name = name;
-		this.costDollars = costDollars;
-		this.costChange = costChange;
+		this.totalPrice = price;
 		this.weight = weight;
 	}
+	public Item(int id,String name, double price, double weight, String type) {
+		super();
+		this.id=id;
+		this.name = name;
+		this.totalPrice = price;
+		this.weight = weight;
+		this.type=type;
+	}
+	
+	public Item(String name,double price, double weight) {
+		super();
+		this.name = name;
+		this.totalPrice = price;
+		this.weight = weight;
+	}
+	
 	
 	public Item(String name) {
 		super();
 		this.name = name;
 	}
-	
-	//takes the set variables for the cost as a dollar and cost of change and sets it to the total cost
-	public void setTotalCost() {
-		String string = costDollars + "." + costChange;
-		double value = Double.parseDouble(string);
-		totalCost = value;
-	}
-	
-	
 
 	@Override
 	public String toString() {
-		return "Item [id=" + id + ", name=" + name + ", totalCost=" + totalCost + ", weight=" + weight + ", type="
+		return "Item [id=" + id + ", name=" + name + ", totalPrice=" + totalPrice + ", weight=" + weight + ", type="
 				+ type + "]";
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Item other = (Item) obj;
-		return costChange == other.costChange && costDollars == other.costDollars && id == other.id
-				&& Objects.equals(name, other.name)
-				&& Double.doubleToLongBits(totalCost) == Double.doubleToLongBits(other.totalCost)
-				&& Objects.equals(type, other.type)
-				&& Double.doubleToLongBits(weight) == Double.doubleToLongBits(other.weight);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(costChange, costDollars, id, name, totalCost, type, weight);
-	}
+	
 	
 	
 }
