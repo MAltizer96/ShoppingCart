dadb0b4ae5aacb2a666438d5aaa58f253e28b298
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2022-05-28 10:51:27 -0400

    Update Item.java
    -removed lombok @data annotation
    -added lombok @getters and @setters annotation to variable's that require it
    -added the getPrice method to return a number to only the hundredth decimal point

--- ShoppingCartMVC/src/main/java/com/main/item/model/Item.java
+++ ShoppingCartMVC/src/main/java/com/main/item/model/Item.java
@@ -1,6 +1,8 @@
 package com.main.item.model;
 
 
+
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,10 +10,11 @@ import javax.persistence.Id;
 
 import org.springframework.stereotype.Component;
 
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+
 @Entity
 @NoArgsConstructor
 @Component
@@ -19,11 +22,10 @@ public class Item {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
-	private String name;	
-	private double price;
-	
-	private String type;
+	@Getter @Setter private int id;
+	@Getter @Setter private String name;	
+	private double price;	
+	@Getter @Setter private String type;
 	
 	public Item(int id,String name, double price) {
 		super();
@@ -40,24 +42,31 @@ public class Item {
 		
 		this.type=type;
 	}
-	
 	public Item(String name,double price) {
 		super();
 		this.name = name;
 		this.price = price;
 		
 	}
-	
-	
+		
 	public Item(String name) {
 		super();
 		this.name = name;
 	}
-
+		
 	@Override
 	public String toString() {
 		return "Item [id=" + id + ", name=" + name + ", price=" + price  + ", type="
 				+ type + "]";
+	}
+	
+	public double getPrice() {
+		Double returnDouble = Math.round(price*100.0)/100.0;
+		return returnDouble;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
 	}
 
 	
